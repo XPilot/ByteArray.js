bf9ecacc8e296972ad5e93500f7aea8ce11c83bf
Zaseth <daanverkooijen01@gmail.com>
2018-04-10 15:57:01 +0200

    Add files via upload

--- RawByteArray.js
+++ RawByteArray.js
@@ -31,6 +31,137 @@ class RawByteArray {
 		}
 	}
 
+	checkIEEE754 (buf, value, offset, ext, max, min) {
+		if (offset + ext > buf.length) {
+			throw new RangeError('Index out of range')
+		}
+		if (offset < 0) {
+			throw new RangeError('Index out of range')
+		}
+	}
+
+	checkRrs (buf, value, offset) {
+		if (typeof offset === "undefined") {
+			this.offset = offset
+		}
+		if (typeof value !== "number" || value % 1 !== 0) {
+			throw TypeError("Illegal value: " + value + " (not an integer)")
+		}
+		if (typeof offset !== "number" || offset % 1 !== 0) {
+			throw TypeError("Illegal offset: " + offset + " (not an integer)")
+		}
+		if (offset < 0 || offset + 0 > buf.length) {
+			throw RangeError("Illegal offset: 0 <= " + offset + " (+" + 0 + ") <= " + buf.length)
+		}
+	}
+
+	readIEEE754 (buffer, offset, isLE, mLen, nBytes) {
+		let e, m
+		let eLen = (nBytes * 8) - mLen - 1
+		let eMax = (1 << eLen) - 1
+		let eBias = eMax >> 1
+		let nBits = -7
+		let i = isLE ? (nBytes - 1) : 0
+		let d = isLE ? -1 : 1
+		let s = buffer[offset + i]
+		i += d
+		e = s & ((1 << (-nBits)) - 1)
+		s >>= (-nBits)
+		nBits += eLen
+		for (; nBits > 0; e = (e * 256) + buffer[offset + i], i += d, nBits -= 8) {}
+		m = e & ((1 << (-nBits)) - 1)
+		e >>= (-nBits)
+		nBits += mLen
+		for (; nBits > 0; m = (m * 256) + buffer[offset + i], i += d, nBits -= 8) {}
+		if (e === 0) {
+			e = 1 - eBias
+		} else if (e === eMax) {
+			return m ? NaN : ((s ? -1 : 1) * Infinity)
+		} else {
+			m = m + Math.pow(2, mLen)
+			e = e - eBias
+		}
+		return (s ? -1 : 1) * m * Math.pow(2, e - mLen)
+	}
+
+	writeIEEE754 (buffer, value, offset, isLE, mLen, nBytes) {
+		let e, m, c
+		let eLen = (nBytes * 8) - mLen - 1
+		let eMax = (1 << eLen) - 1
+		let eBias = eMax >> 1
+		let rt = (mLen === 23 ? Math.pow(2, -24) - Math.pow(2, -77) : 0)
+		let i = isLE ? 0 : (nBytes - 1)
+	    let d = isLE ? 1 : -1
+	    let s = value < 0 || (value === 0 && 1 / value < 0) ? 1 : 0
+	    value = Math.abs(value)
+	    if (isNaN(value) || value === Infinity) {
+	    	m = isNaN(value) ? 1 : 0
+	    	e = eMax
+	    } else {
+	    	e = Math.floor(Math.log(value) / Math.LN2)
+	    	if (value * (c = Math.pow(2, -e)) < 1) {
+	    		e--
+	    		c *= 2
+	    	}
+	    	if (e + eBias >= 1) {
+	    		value += rt / c
+	    	} else {
+	    		value += rt * Math.pow(2, 1 - eBias)
+	    	}
+	    	if (value * c >= 2) {
+	    	  e++
+	    	  c /= 2
+	    	}
+	    	if (e + eBias >= eMax) {
+	    		m = 0
+	    		e = eMax
+	    	} else if (e + eBias >= 1) {
+	    		m = ((value * c) - 1) * Math.pow(2, mLen)
+	    		e = e + eBias
+	    	} else {
+	    		m = value * Math.pow(2, eBias - 1) * Math.pow(2, mLen)
+	    		e = 0
+	    	}
+	    }
+	    for (; mLen >= 8; buffer[offset + i] = m & 0xff, i += d, m /= 256, mLen -= 8) {}
+	    e = (e << mLen) | m
+	    eLen += mLen
+	    for (; eLen > 0; buffer[offset + i] = e & 0xff, i += d, e /= 256, eLen -= 8) {}
+	    buffer[offset + i - d] |= s * 128
+	}
+
+	writeFloat (buf, value, offset, littleEndian) {
+		value = +value
+		offset = offset >>> 0
+		this.checkIEEE754(buf, value, offset, 4, 3.4028234663852886e+38, -3.4028234663852886e+38)
+		this.writeIEEE754(buf, value, offset, littleEndian, 23, 4)
+		return this.offset += 4
+	}
+
+	writeDouble (buf, value, offset, littleEndian) {
+		value = +value
+		offset = offset >>> 0
+		this.checkIEEE754(buf, value, offset, 8, 1.7976931348623157E+308, -1.7976931348623157E+308)
+		this.writeIEEE754(buf, value, offset, littleEndian, 52, 8)
+		return this.offset += 8
+	}
+
+	writeFloatBE (value) {
+		return this.writeFloat(this.buffer, value, this.offset, false)
+	}
+
+	writeDoubleBE (value) {
+		return this.writeDouble(this.buffer, value, this.offset, false)
+	}
+
+	writeString (string) {
+		if (string.length < 0) {
+			string = "Error"
+		}
+		this.writeInt8(string.length)
+		this.buffer.write(string, this.offset, this.offset += string.length, "utf8")
+	}
+
 	writeIntBE (value, byteLength) {
 		value = +value
 		this.offset = this.offset >>> 0
@@ -96,6 +227,7 @@ class RawByteArray {
 	writeInt40BE (value) {
 		value = +value
 		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 5, 0x7FFFFFFFFF, -0x8000000000)
 		let high = Math.floor(value / 0x100000000)
 		let low = value - high * 0x100000000
 		this.writeInt8(high)
@@ -105,6 +237,7 @@ class RawByteArray {
 	writeInt48BE (value) {
 		value = +value
 		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 6, 0x7FFFFFFFFFFF, -0x800000000000)
 		let high = Math.floor(value / 0x100000000)
 		let low = value - high * 0x100000000
 		this.writeInt16BE(high)
@@ -114,6 +247,7 @@ class RawByteArray {
 	writeInt56BE (value) {
 		value = +value
 		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 7, 0x7FFFFFFFFFFFFF, -0x80000000000000)
 		let temp = Math.floor(value / 0x100000000)
 		let high = Math.floor(temp / 0x10000)
 		let mid = temp - high * 0x10000
@@ -126,6 +260,7 @@ class RawByteArray {
 	writeInt64BE (value) {
 		value = +value
 		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 8, 0x7FFFFFFFFFFFFFFF, -0x8000000000000000)
 		let high = Math.floor(value / 0x100000000)
 		let low = value - high * 0x100000000
 		this.writeInt32BE(high)
@@ -188,6 +323,7 @@ class RawByteArray {
 	writeUInt40BE (value) {
 		value = +value
 		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 5, 0xFFFFFFFFFF, 0)
 		let high = Math.floor(value / 0x100000000)
 		let low = value - high * 0x100000000
 		this.writeUInt8(high)
@@ -197,6 +333,7 @@ class RawByteArray {
 	writeUInt48BE (value) {
 		value = +value
 		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 6, 0xFFFFFFFFFFFF, 0)
 		let high = Math.floor(value / 0x100000000)
 		let low = value - high * 0x100000000
 		this.writeUInt16BE(high)
@@ -206,6 +343,7 @@ class RawByteArray {
 	writeUInt56BE (value) {
 		value = +value
 		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 7, 0xFFFFFFFFFFFFFF, 0)
 		let temp = Math.floor(value / 0x100000000)
 		let high = Math.floor(temp / 0x10000)
 		let mid = temp - high * 0x10000
@@ -218,10 +356,29 @@ class RawByteArray {
 	writeUInt64BE (value) {
 		value = +value
 		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 8, 0xFFFFFFFFFFFFFFFF, 0)
 		let high = Math.floor(value / 0x100000000)
 		let low = value - high * 0x100000000
 		this.writeUInt32BE(high)
 		this.writeUInt32BE(low)
+	}
+
+	readFloatBE () {
+		this.offset = this.offset >>> 0
+		this.checkOffset(this.offset, 4, this.buffer.length)
+		return this.readIEEE754(this.buffer, this.offset, false, 23, 4)
+	}
+
+	readDoubleBE () {
+		this.offset = this.offset >>> 0
+		this.checkOffset(this.offset, 8, this.buffer.length)
+		return this.readIEEE754(this.buffer, this.offset, false, 52, 8)
+	}
+
+	readString () {
+		let length = this.readUInt8()
+		this.offset += length
+		return this.buffer.toString("utf8", 1, this.offset)
 	}
 
 	readIntBE (byteLength) {
@@ -380,6 +537,237 @@ class RawByteArray {
 		this.checkOffset(this.offset, 8, this.buffer.length)
 		let val = this.readUInt32BE() * (1 << 16) * (1 << 16) + this.readUInt32BE()
 		return val
+	}
+
+	writeFloatLE (value) {
+		return this.writeFloat(this.buffer, value, this.offset, true)
+	}
+
+	writeDoubleLE (value) {
+		return this.writeDouble(this.buffer, value, this.offset, true)
+	}
+
+	writeIntLE (value, byteLength) {
+		value = +value
+		this.offset = this.offset >>> 0
+		let limit = Math.pow(2, (8 * byteLength) - 1)
+		this.checkInt(this.buffer, value, this.offset, byteLength, limit - 1, -limit)
+		let i = 0
+		let mul = 1
+		let sub = 0
+		this.buffer[this.offset] = (value & 0xFF)
+		while (++i < byteLength && (mul *= 0x100)) {
+			if (value < 0 && sub === 0 && this.buffer[this.offset + i - 1] !== 0) {
+				sub = 1
+			}
+			this.buffer[this.offset + i] = ((value / mul) >> 0) - sub & 0xFF
+		}
+		return this.offset += byteLength
+	}
+
+	writeInt16LE (value) {
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 2, 0x7FFF, -0x8000)
+		this.buffer[this.offset] = (value & 0xFF)
+		this.buffer[this.offset + 1] = (value >>> 8)
+		return this.offset += 2
+	}
+
+	writeInt24LE (value) {
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value. this.offset, 3, 0x7FFFFF, -0x800000)
+		this.buffer[this.offset] = (value & 0xFF)
+		this.buffer[this.offset + 1] = (value >>> 8)
+		this.buffer[this.offset + 2] = (value >>> 16)
+		return this.offset += 3
+	}
+
+	writeInt32LE (value) {
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 4, 0x7FFFFFFF, -0x80000000)
+		this.buffer[this.offset] = (value & 0xFF)
+		this.buffer[this.offset + 1] = (value >>> 8)
+		this.buffer[this.offset + 2] = (value >>> 16)
+		this.buffer[this.offset + 3] = (value >>> 24)
+		return this.offset += 4
+	}
+
+	writeInt40LE (value) {
+		let SHIFT_RIGHT_32 = 1 / (1 << 16) * (1 << 16)
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 5, 0x7FFFFFFFFF, -0x8000000000)
+		this.writeInt32LE(value & -1)
+		this.writeInt8(Math.floor(value * SHIFT_RIGHT_32))
+	}
+
+	writeInt48LE (value) {
+		let SHIFT_RIGHT_32 = 1 / (1 << 16) * (1 << 16)
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 6, 0x7FFFFFFFFFFF, -0x800000000000)
+		this.writeInt32LE(value & -1)
+		this.writeInt16LE(Math.floor(value * SHIFT_RIGHT_32))
+	}
+
+	writeInt56LE (value) {
+		let SHIFT_RIGHT_32 = 1 / (1 << 16) * (1 << 16)
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 7, 0x7FFFFFFFFFFFFF, -0x80000000000000)
+		if (value < 0x80000000000000) {
+			this.writeInt32LE(value & -1)
+			let high = Math.floor(value * SHIFT_RIGHT_32)
+			this.writeUInt16LE(high & 0xFFFF)
+			this.writeInt8(high >> 16)
+		} else {
+			this.buffer[this.offset] = 0xFF
+			this.buffer[this.offset + 1] = 0xFF
+			this.buffer[this.offset + 2] = 0xFF
+			this.buffer[this.offset + 3] = 0xFF
+			this.buffer[this.offset + 4] = 0xFF
+			this.buffer[this.offset + 5] = 0xFF
+			this.buffer[this.offset + 6] = 0x7F
+		}
+	}
+
+	writeInt64LE (value) {
+		let SHIFT_RIGHT_32 = 1 / (1 << 16) * (1 << 16)
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 8, 0x7FFFFFFFFFFFFFFF, -0x8000000000000000)
+		if (value < 0x8000000000000000) {
+			this.writeInt32LE(value & -1)
+			this.writeInt32LE(Math.floor(value * SHIFT_RIGHT_32))
+		} else {
+			this.buffer[this.offset] = 0xFF
+			this.buffer[this.offset + 1] = 0xFF
+			this.buffer[this.offset + 2] = 0xFF
+			this.buffer[this.offset + 3] = 0xFF
+			this.buffer[this.offset + 4] = 0xFF
+			this.buffer[this.offset + 5] = 0xFF
+			this.buffer[this.offset + 6] = 0xFF
+			this.buffer[this.offset + 7] = 0x7F
+		}
+	}
+
+	writeUIntLE (value, byteLength) {
+		value = +value
+		this.offset = this.offset >>> 0
+		byteLength = byteLength >>> 0
+		let maxBytes = Math.pow(2, * * byteLength) - 1
+		this.checkInt(this.buffer, value, this.offset, byteLength, maxBytes, 0)
+		let mul = 1
+		let i = 0
+		this.buffer[this.offset] = (value & 0xFF)
+		while (++i < byteLength && (mul *= 0x100)) {
+			this.buffer[this.offset + i] = (value / mul) & 0xFF
+		}
+		return this.offset += byteLength
+	}
+
+	writeUInt16LE (value) {
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 2, 0xFFFF, 0)
+		this.buffer[this.offset] = (value & 0xFF)
+		this.buffer[this.offset + 1] = (value >>> 8)
+		return this.offset += 2
+	}
+
+	writeUInt24LE (value) {
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 3, 0xFFFFFF, 0)
+		this.buffer[this.offset + 2] = (value >>> 16)
+		this.buffer[this.offset + 1] = (value >>> 8)
+		this.buffer[this.offset] = (value & 0xFF)
+		return this.offset += 3
+	}
+
+	writeUInt32LE (value) {
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 4, 0xFFFFFFFF, 0)
+		this.buffer[this.offset + 3] = (value >>> 24)
+		this.buffer[this.offset + 2] = (value >>> 16)
+		this.buffer[this.offset + 1] = (value >>> 8)
+		this.buffer[this.offset] = (value & 0xFF)
+		return this.offset += 4
+	}
+
+	writeUInt40LE (value) {
+		let SHIFT_RIGHT_32 = 1 / (1 << 16) * (1 << 16)
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 5, 0xFFFFFFFFFF, 0)
+		this.writeInt32LE(value & -1)
+		this.writeUInt8(Math.floor(value * SHIFT_RIGHT_32))
+	}
+
+	writeUInt48LE (value) {
+		let SHIFT_RIGHT_32 = 1 / (1 << 16) * (1 << 16)
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 6, 0xFFFFFFFFFFFF, 0)
+		this.writeInt32LE(value & -1)
+		this.writeUInt16LE(Math.floor(value * SHIFT_RIGHT_32))
+	}
+
+	writeUInt56LE (value) {
+		let SHIFT_RIGHT_32 = 1 / (1 << 16) * (1 << 16)
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 7, 0xFFFFFFFFFFFFFF, 0)
+		if (value < 0x100000000000000) {
+			this.writeInt32LE(value & -1)
+			let hi = Math.floor(value * SHIFT_RIGHT_32)
+			this.writeUInt16LE(hi & 0xFFFF)
+			this.writeUInt8(hi >>> 16)
+		} else {
+			this.buffer[this.offset] = 0xFF
+			this.buffer[this.offset + 1] = 0xFF
+			this.buffer[this.offset + 2] = 0xFF
+			this.buffer[this.offset + 3] = 0xFF
+			this.buffer[this.offset + 4] = 0xFF
+			this.buffer[this.offset + 5] = 0xFF
+			this.buffer[this.offset + 6] = 0xFF
+		}
+	}
+
+	writeUInt64LE (value) {
+		let SHIFT_RIGHT_32 = 1 / (1 << 16) * (1 << 16)
+		value = +value
+		this.offset = this.offset >>> 0
+		this.checkInt(this.buffer, value, this.offset, 8, 0XFFFFFFFFFFFFFFFF, 0)
+		if (value < 0x10000000000000000) {
+			this.writeInt32LE(value & -1)
+			this.writeUInt32LE(Math.floor(value * SHIFT_RIGHT_32))
+		} else {
+			this.buffer[this.offset] = 0xFF
+			this.buffer[this.offset + 1] = 0xFF
+			this.buffer[this.offset + 2] = 0xFF
+			this.buffer[this.offset + 3] = 0xFF
+			this.buffer[this.offset + 4] = 0xFF
+			this.buffer[this.offset + 5] = 0xFF
+			this.buffer[this.offset + 6] = 0xFF
+			this.buffer[this.offset + 7] = 0xFF
+		}
+	}
+
+	readFloatLE () {
+		this.offset = this.offset >>> 0
+		this.checkOffset(this.offset, 4, this.buffer.length)
+		return this.readIEEE754(this.buffer, this.offset, true, 23, 4)
+	}
+
+	readDoubleLE () {
+		this.offset = this.offset >>> 0
+		this.checkOffset(this.offset, 8, this.buffer.length)
+		return this.readIEEE754(this.buffer, this.offset, true, 52, 8)
 	}
 }
 
